5b4c159166a14c780a403f8bc3439d64bafea85b
WayneLi1063 <weileli@uw.edu>
2021-06-03 01:46:40 -0700

    minor fixes

--- clients/src/FilteredCardList.js
+++ clients/src/FilteredCardList.js
@@ -235,7 +235,6 @@ export default class FilteredCardList extends React.Component {
 
     // renders the filter form
     render() {
-        console.log(this.props.cards)
         let listOfCards = [];
         let content = null;
         if (this.state.cardList) {
